6a6eeb5c35d4da29e783e3920ddf6bd557843bf1
Shawn McCabe <smccabe@acromediainc.com>
2019-06-10 16:32:21 -0700

    limit font tokens to TEXT only

--- bin/functions/setupFontTokens.mjs
+++ bin/functions/setupFontTokens.mjs
@@ -6,11 +6,14 @@ export function setupFontTokens(frame) {
 		let fontObject = {};
 
 		frame.children.forEach(type => {
-			let name = camelize(type.name);
-			name = formatName(name);
-			const font = type.style.fontPostScriptName;
+      if(type.type === 'TEXT') {
+        console.log(type);
+        let name = camelize(type.name);
+        name = formatName(name);
+        const font = type.style.fontPostScriptName;
 
-			fontObject[name] = font;
+        fontObject[name] = font;
+      }
 		});
 
 		return fontObject;
